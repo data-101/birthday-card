53a4008c260fc32903607e3bcee8dff995c3f71a
unknown <akashnainani9@gmail.com>
2020-08-17 21:29:38 +0530

    Updates

---
+++ precache-manifest.e6e74dd0af6ff8cb95ac4c40414b9c7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc88a91ebd3f043562beacb97b0ba48a",
+    "url": "/birthday-card/index.html"
+  },
+  {
+    "revision": "a3342010691363e23bf5",
+    "url": "/birthday-card/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "59f13e921e3750055133",
+    "url": "/birthday-card/static/js/2.6251dc9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birthday-card/static/js/2.6251dc9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3342010691363e23bf5",
+    "url": "/birthday-card/static/js/main.cda53103.chunk.js"
+  },
+  {
+    "revision": "755f7f94f2545570cb1a",
+    "url": "/birthday-card/static/js/runtime-main.8ce2e272.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/birthday-card/static/media/logo.5d5d9eef.svg"
+  }
+]);
